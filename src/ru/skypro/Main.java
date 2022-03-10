1177f8784e463242635ec77cfdd0fd3bc5e0ab47
Андрей <gosudarev_a@mail.ru>
2022-03-10 07:44:45 +0300

    Домашка по циклам. Обработка комментария по 3-й задаче.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,15 +7,15 @@ public class Main {
         System.out.println("ЗАДАЧА №1");
 
 
-        int point=1;
-        while (point<=10) {
+        int point = 1;
+        while (point <= 10) {
             System.out.print(point + " ");
             point++;
         }
-            System.out.println();
-            for (int i = 10; i > 0; i--) {
-                System.out.print(i + " ");
-            }
+        System.out.println();
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
 
 
         System.out.println();
@@ -23,7 +23,7 @@ public class Main {
         System.out.println("ЗАДАЧА №2");
 
         int firstFriday = 4; //первая пятница марта - 4 марта 2022
-        for (int i = 4; i < 31; i=i+7) {
+        for (int i = 4; i < 31; i = i + 7) {
             System.out.println("Сегодня пятница," + i + "-е число. Необходимо подготовить отчет.");
         }
 
@@ -31,15 +31,21 @@ public class Main {
         System.out.println();
         System.out.println("ЗАДАЧА №3");
 
-        int yearBefore = 2022-200;
-        int yearAfter = 2022+100;
+        int currentYear=2022;      // добавлено
+        int yearBefore = currentYear - 200;
+        int yearAfter = currentYear + 100;
 
-        for (int i = 0; i < 2022; i=i+79) {
-            while (i>yearBefore && i<yearAfter) {
+        for (int i = 0; i < yearAfter; i = i + 79) {      // немного изменено
+            if (i > yearBefore && i < yearAfter) {  // немного изменено
                 System.out.println(i);
-                i=i+79;
+            } else if (i > yearBefore && i < yearAfter) {  // добавлено
+                System.out.println(i);
+            } else if (i > yearBefore && i < yearAfter) { // добавлено
+                System.out.println(i);
             }
+
         }
     }
 }
+
 
